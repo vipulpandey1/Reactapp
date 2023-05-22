e0a605a4456a799a2ff43e0588558a5f7c812371
Vipul <36097069+vipulpandey1@users.noreply.github.com>
2023-05-22 13:08:52 +0530

    Update App.js

--- aesthisia-demo/src/App.js
+++ aesthisia-demo/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Aesthisia, You are almost there!
+          Welcome, You are almost there!
         </p>
         <a
           className="App-link"
